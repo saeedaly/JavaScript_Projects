2d1425218064907d1ce9c373f17f713b9d98e506
Saeed Ali Abdullah Ahmed <saeedaly11@gmail.com>
2025-07-26 22:55:43 +0300

    changed the behaviour to let the user choose the range of the random numbers

--- randomNumbers/main.js
+++ randomNumbers/main.js
@@ -1,11 +1,14 @@
 
 const rollbtn = document.getElementById("rollbtn");
 const counterlabel = document.getElementById("counterlabel");
+const maxValue = document.getElementById("max");
+const minValue = document.getElementById("min");
 let randomNumber;
 
 rollbtn.onclick = function(){
-    
-    randomNumber = Math.floor(Math.random() * 10);
+    max = Number(maxValue.value);
+    min = Number(minValue.value);
+    randomNumber = Math.floor(Math.random() * (max - min +1)) + min;
     counterlabel.textContent = randomNumber;
 }
 
